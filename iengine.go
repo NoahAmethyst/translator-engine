8108faf5f7f7cf45673c2f01c097fa847e430eb9
MZH <1066840101@qq.com>
2023-04-28 21:13:23 +0800

    -- fix  - optimize code

--- iengine.go
+++ iengine.go
@@ -2,7 +2,7 @@ package translator_engine
 
 import (
 	"context"
-	"reflect"
+	"errors"
 )
 
 type factory struct {
@@ -31,46 +31,9 @@ type ITransEngine interface {
 
 func TransText(src, from, to string, eng ITransEngine, scene ...Scene) (*TransResult, error) {
 
-	_eng := reflect.ValueOf(eng)
-
-	fLanProxy := _eng.MethodByName("LanCodeIn")
-
-	lanFrom := fLanProxy.Call([]reflect.Value{reflect.ValueOf(from)})
-
-	lanTo := fLanProxy.Call([]reflect.Value{reflect.ValueOf(to)})
-
-	var results []reflect.Value
-
-	if len(scene) > 0 {
-		results = _eng.MethodByName("TransTextDirect").
-			Call([]reflect.Value{
-				reflect.ValueOf(src),
-				lanFrom[0],
-				lanTo[0],
-				reflect.ValueOf(scene[0]),
-			})
-	} else {
-		results = _eng.MethodByName("TransTextDirect").
-			Call([]reflect.Value{
-				reflect.ValueOf(src),
-				lanFrom[0],
-				lanTo[0],
-			})
+	if eng == nil {
+		return nil, errors.New("unsupported engine")
 	}
 
-	var resultsInterface []interface{}
-	for _, v := range results {
-		resultsInterface = append(resultsInterface, v.Interface())
-	}
-
-	var result *TransResult
-	var err error
-	if resultsInterface[0] != nil {
-		result = resultsInterface[0].(*TransResult)
-	}
-	if resultsInterface[1] != nil {
-		err = resultsInterface[1].(error)
-	}
-
-	return result, err
+	return eng.TransText(src, from, to, scene...)
 }
